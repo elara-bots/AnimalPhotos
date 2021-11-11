1b2cc019ad61accad89ba2b388796fa6553f16f7
Elara Services <elaradiscordbot@gmail.com>
2021-11-10 23:01:14 -0800

    Add the refresh button

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,5 @@
 import { verify } from "./verify";
-import { InteractionType, InteractionResponseType, APIInteractionResponse, APIApplicationCommandInteraction } from "discord-api-types/v9";
+import { InteractionType, InteractionResponseType, APIInteractionResponse, APIEmbed, APIApplicationCommandInteraction, APIButtonComponent } from "discord-api-types/v9";
 import { APIPingInteraction } from 'discord-api-types/payloads/v9/_interactions/ping'
 
 interface ImgStatus {
@@ -8,15 +8,27 @@ interface ImgStatus {
   image?: string
 }
 
+function component(components: APIButtonComponent[]) {
+  return [ { type: 1, components } ]
+}
+
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url)
   switch(url.pathname) {
       case "/interactions": {
         if (!request.headers.get('X-Signature-Ed25519') || !request.headers.get('X-Signature-Timestamp')) return Response.redirect('https://discord.com')
         if (!await verify(request)) return new Response('', { status: 401 })
-        const interaction = await request.json() as APIPingInteraction | APIApplicationCommandInteraction
+        const interaction = await request.json() as APIPingInteraction | APIApplicationCommandInteraction | any;
         if (interaction.type === InteractionType.Ping) return respond({ type: InteractionResponseType.Pong });
-        const name: string = interaction.data.name.toLowerCase();
+        const userId = interaction.member!.user.id ?? interaction.user!.id;
+        if (!userId) return error(`❌ Unable to find your user ID`);
+        let edit = false,
+            name = "";
+        if (interaction.type === InteractionType.MessageComponent) {
+          name = interaction.data.custom_id.split(":")[0];
+          edit = true;
+          if (interaction.data.custom_id.split(":")[1] !== userId) return error(`❌ You didn't run this command!`, false);
+        } else name = interaction.data.name.toLowerCase();
         if (!name) return respond({ 
           type: InteractionResponseType.ChannelMessageWithSource, 
           data: {
@@ -45,42 +57,43 @@ export async function handleRequest(request: Request): Promise<Response> {
               ]
             }
           });
-          case "cat": return int("cats", "photos", "🐈 Cat!");
-          case "dog": return int("dogs", "photos", "🐕 Dog!");
-          case "fox": return int("fox", "photos", "🦊 Fox!");
-          case "pandas": return int("panda", "photos", "🐼 Panda!");
-          case "panda": return int("panda", "special", "🐈 Panda!");
-          case "penguin": return int("penguin", "photos", "🐧 Penguin!");
-          case "otter": return int("otter", "photos", "🦦 Otter!");
-          case "bird": return int("bird", "photos", "🐦 Birb!");
-          case "bunny": return int("bunny", "photos", "🐇 Bunny!");
-          case "duck": return int("duck", "photos", "🦆 Quack!");
-          case "redpanda": return int("redpanda", "photos", "<:RedPanda:849665956761305200> Red Panda!");
-          case "shibe": return int("shibe", "photos", "Shiba!");
-          case "pj": return int("pj", "special", "🐈 PJ!");
-          case "tiggy": return int("tiggy", "special", "🐈 Tiggy!");
-          case "sylvester": return int("sylvester", "special", "🐈 Sylvester!");
-          case "husky": return int("husky", "photos", "🐕 Husky!");
-          case "pug": return int("pug", "photos", "🐕 Pug!");
+          case "cat": case "cats": return int("cats", "photos", "🐈 Cat!", edit, userId);
+          case "dog": case "dogs": return int("dogs", "photos", "🐕 Dog!", edit, userId);
+          case "fox": return int("fox", "photos", "🦊 Fox!", edit, userId);
+          case "pandas": return int("pandas", "photos", "🐼 Panda!", edit, userId);
+          case "panda": return int("panda", "special", "🐈 Panda!", edit, userId);
+          case "penguin": return int("penguin", "photos", "🐧 Penguin!", edit, userId);
+          case "otter": return int("otter", "photos", "🦦 Otter!", edit, userId);
+          case "bird": return int("bird", "photos", "🐦 Birb!", edit, userId);
+          case "bunny": return int("bunny", "photos", "🐇 Bunny!", edit, userId);
+          case "duck": return int("duck", "photos", "🦆 Quack!", edit, userId);
+          case "redpanda": return int("redpanda", "photos", "<:RedPanda:849665956761305200> Red Panda!", edit, userId);
+          case "shibe": return int("shibe", "photos", "Shiba!", edit, userId);
+          case "pj": return int("pj", "special", "🐈 PJ!", edit, userId);
+          case "tiggy": return int("tiggy", "special", "🐈 Tiggy!", edit, userId);
+          case "sylvester": return int("sylvester", "special", "🐈 Sylvester!", edit, userId);
+          case "husky": return int("husky", "photos", "🐕 Husky!", edit, userId);
+          case "pug": return int("pug", "photos", "🐕 Pug!", edit, userId);
           case "aww": {
             const res = await reddit("aww");
-            if (!res.status || !res.image) return error(res!.message ?? "Unknown Issue while trying to fetch the subreddit.");
+            if (!res.status || !res.image) return error(res!.message ?? "Unknown Issue while trying to fetch the subreddit.", edit);
             return respond({ 
-              type: InteractionResponseType.ChannelMessageWithSource, 
+              type: edit ? InteractionResponseType.UpdateMessage : InteractionResponseType.ChannelMessageWithSource, 
               data: {
                 content: res.image,
+                components: component([ { type: 2, custom_id: `aww:${userId}`, style: 3, emoji: { id: "849713246813945876" } } ])
               } 
             })
           }
         }
-        return error(`❌ The command you tried is a work in progress.`);
+        return error(`❌ The command you tried is a work in progress.`, edit);
       };
   };
   return new Response(`request method: ${request.method}`)
 }
 
-const error = (message: string) => respond({
-  type: InteractionResponseType.ChannelMessageWithSource,
+const error = (message: string, edit?: boolean) => respond({
+  type: edit ? InteractionResponseType.UpdateMessage : InteractionResponseType.ChannelMessageWithSource,
   data: {
     embeds: [
       {
@@ -107,21 +120,21 @@ const getPhoto = async (type: string, name: string): Promise<ImgStatus> => {
     }
 };
 
-const int = async (name: string, type: string, title: string): Promise<any> => {
+const int = async (name: string, type: string, title: string, edit?: boolean, userId?: string): Promise<any> => {
     const r = await getPhoto(type, name);
     if (!r.status || !r.image) return respond({
-      type: InteractionResponseType.ChannelMessageWithSource,
+      type: edit ? InteractionResponseType.UpdateMessage : InteractionResponseType.ChannelMessageWithSource,
       data: {
         content: `❌ ${r.message}`,
         flags: 1 << 6
       }
     });
-    return image(r.image, title);
+    return image(r.image, title, edit, name, `${userId}`);
 };
 
-const image = (img: string, title?: string) => {
+const image = (img: string, title?: string, edit?: boolean, name?: string, userId?: string) => {
   return respond({
-    type: InteractionResponseType.ChannelMessageWithSource,
+    type: edit ? InteractionResponseType.UpdateMessage : InteractionResponseType.ChannelMessageWithSource,
     data: {
       embeds: [
         {
@@ -130,7 +143,8 @@ const image = (img: string, title?: string) => {
           image: { url: img },
           color: 2409471
         }
-      ]
+      ],
+      components: component([ { type: 2, custom_id: `${name}:${userId}`, style: 3, emoji: { id: "849713246813945876" } } ])
     }
   })
 }
